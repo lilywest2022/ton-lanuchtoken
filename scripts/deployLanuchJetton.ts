208a4dc120187cdcb1e0ed08e50a96c27c118be1
liywest <Zhuanz@1deMacBook-Pro.local>
2024-06-29 18:22:32 +0800

    contract

--- scripts/deployLanuchJetton.ts
+++ scripts/deployLanuchJetton.ts
@@ -1,5 +1,5 @@
 import { Address, toNano } from '@ton/core';
-import { LanuchJetton } from '../wrappers/LanuchJetton';
+import { LanuchJetton,CreateNewPoolConfig } from '../wrappers/LanuchJetton';
 import { NetworkProvider } from '@ton/blueprint';
 import { buildOnchainMetadata } from "../utils/jetton-helpers";
 
@@ -9,13 +9,21 @@ const jettonParams = {
     symbol: "TJ",
     image: "https://avatars.githubusercontent.com/u/115602512?s=96&v=4",
 };
+let  CreateNewPoolConfig :CreateNewPoolConfig= {
+    $$type: "CreateNewPoolConfig",
+    swapRouterAddress:Address.parse("EQBsGx9ArADUrREB34W-ghgsCgBShvfUr4Jvlu-0KGc33Rbt"),
+    proxyTon: Address.parse("kQAcOvXSnnOhCdLYc6up2ECYwtNNTzlmOlidBeCs5cFPV7AM"),
+    tonValueAddMeme:toNano("0.5"),
+    tonValueAddPton:toNano("0.3"),
+    routerPtonWallet:Address.parse("kQCdC2b1GG1saybYxCwRfEqr4WlOexsQIcYcfMYObk_477vs"),
+}
+
 
 export async function run(provider: NetworkProvider) {
     const owner = provider.sender().address as Address;
-    const lanuchjetton = provider.open(await LanuchJetton.fromInit(owner, BigInt(50)));
+    const lanuchjetton = provider.open(await LanuchJetton.fromInit(owner, BigInt(50),true));
 
     let content = buildOnchainMetadata(jettonParams);
-    let max_supply = toNano(123456766689011);
 
     await lanuchjetton.send(
         provider.sender(),
@@ -24,10 +32,14 @@ export async function run(provider: NetworkProvider) {
         },
         {
             $$type: 'CreateNewJetton',
-            owner: owner,
+            token_owner: owner,
             content: content,
-            max_supply: max_supply,
+            max_supply: toNano(1000000000000),
             rate: BigInt(50),
+            lanuch_rate:BigInt(20),
+            isLanuch:true,
+            config:CreateNewPoolConfig
+
         }
     );
     console.log('Transaction sent')
